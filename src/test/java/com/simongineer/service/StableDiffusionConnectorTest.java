772cc54bc9e65aae6a7604e17e82316a2cd974b9
simongineer <simongineer@srahm.de>
2023-10-26 00:04:08 +0200

    Refactored StableDiffusionConnector. Made #generateTxt2ImgAsync async.
    Signed-off-by: simongineer <simongineer@srahm.de>

--- src/test/java/com/simongineer/service/StableDiffusionConnectorTest.java
+++ src/test/java/com/simongineer/service/StableDiffusionConnectorTest.java
@@ -9,6 +9,9 @@ import com.simongineer.diffuse_match.utils.Local;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
 /**
  * JUnit 5 test class for the StableDiffusionConnector class.
  */
@@ -24,8 +27,12 @@ public class StableDiffusionConnectorTest {
      */
     @Test
     public void testGenerateTxt2Img() {
-        byte[] imageBytes = StableDiffusionConnector.generateTxt2Img(prompt);
-        assertNotNull(imageBytes);
-        assertTrue(imageBytes.length > 0);
+        CompletableFuture<byte[]> futureImage = StableDiffusionConnector.generateTxt2ImgAsync(prompt);
+        try {
+            assertNotNull(futureImage.get());
+            assertTrue(futureImage.get().length > 0);
+        } catch (InterruptedException | ExecutionException e) {
+            e.printStackTrace();
+        }
     }
 }
