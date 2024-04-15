1b5befc66db2bbe6a00a32beb36e822a87c60511
simongineer <simongineer@srahm.de>
2024-04-15 21:44:11 +0200

    s

--- src/main/java/com/simongineer/diffuse_match/service/StableDiffusionConnector.java
+++ src/main/java/com/simongineer/diffuse_match/service/StableDiffusionConnector.java
@@ -12,6 +12,7 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import com.simongineer.diffuse_match.beans.Prompt;
+import com.simongineer.diffuse_match.utils.Local;
 
 import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
@@ -99,6 +100,8 @@ public abstract class StableDiffusionConnector {
             byte[] imageBytes = java.util.Base64.getDecoder().decode((jsonElement.getAsString()));
             imgList.add(imageBytes);
         });
+        Local.saveGeneratedImage(imgList.get(0));
+        Local.saveGeneratedImage(imgList.get(1));
         return imgList.get(0);
     }
 }
